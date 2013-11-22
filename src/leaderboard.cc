9400f1b11cb117aaf923efc4b30180332a14e303
martinsk <msk@tigertext.com>
2013-11-22 11:52:27 -0800

    pagenation

--- src/leaderboard.cc
+++ src/leaderboard.cc
@@ -1,35 +1,90 @@
+
 #include <iostream>
+#include <algorithm>
 #include "leaderboard.hh"
 #include "event_array.hh"
 
 
 void
 leaderboard::add(std::string elm, long score) {
-  if(contains(elm)) {
-    remove(elm);
+  if(!contains(elm)) {
+    board.push_back(make_pair(score, elm));
+    idx_lookup[elm] = board.size()-1;
+    bubble_up(idx_lookup[elm]);
+  } 
+  else {
+    long oldscore = board[idx_lookup[elm]].first;
+    board[idx_lookup[elm]].first = score;
+    if(oldscore < score) bubble_up(idx_lookup[elm]);
+    else bubble_down(idx_lookup[elm]);
   }
-
-  score_map[elm] = score;
-  board.insert(std::make_pair(-score, elm));
 }
 
 void 
 leaderboard::remove(std::string elm){
-  unsigned long old_score = score_map[elm];
-  board.erase(std::make_pair(-old_score, elm));
-  //  print();
+  board[idx_lookup[elm]].first = -1;
+  bubble_down(idx_lookup[elm]);
+  idx_lookup.erase(elm);
+  board.pop_back();
 }
 
 
 bool
 leaderboard::contains(const count_name_t elm) const {
-  return score_map.count(elm) != 0;
+  return idx_lookup.count(elm) != 0;
 }
+
+unsigned int
+leaderboard::size() const {
+  return board.size();
+}
+
+std::vector<std::pair< long, count_name_t> >
+leaderboard::get_range(idx_t from, idx_t to) const {
+  std::vector<std::pair<long, count_name_t> > ret;
+  for(idx_t idx = std::min(from, (idx_t)board.size()-1);
+      idx < std::min(to, (idx_t)board.size()); idx++){
+    ret.push_back(board[idx]);
+  }
+  return ret;
+}
+
 
 
 void leaderboard::print() {
   for(auto p : board) {
     std::cout << p.second << ":" << p.first << std::endl;  
   }
+}
+
+void leaderboard::bubble_up(idx_t idx) {
+  if(idx != 0) {
+    if (board[idx].first > board[idx -1].first) {
+      swap(idx, idx-1);
+      bubble_up(idx-1);
+    }
+  }
+}
+
+
+
+void leaderboard::bubble_down(idx_t idx)  {
+  if(idx != board.size() -1) {
+    if (board[idx].first < board[idx +1].first) {
+      swap(idx, idx+1);
+      bubble_down(idx+1);
+    }
+  }
+}
+
+
+
+
+void leaderboard::swap(idx_t idx1, idx_t idx2)  {
+  idx_lookup[board[idx1].second] = idx2;
+  idx_lookup[board[idx2].second] = idx1;
+  auto tmp = board[idx1];
+  board[idx1] = board[idx2];
+  board[idx2] = tmp;   
 }
 
